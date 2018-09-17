2232c9436a7b3bee20cdb1a924efa399860a0662
nambi <nambiorzota@gmail.com>
2018-09-17 19:46:43 +0530

    Sep_17_2018

--- RAN/src/main/Demo.java
+++ RAN/src/main/Demo.java
@@ -1,13 +1,23 @@
 package main;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Iterator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.common.collect.Iterators;
 
 public class Demo {
-	public static void main(String args[]) throws InterruptedException {
-		Set<Integer> s = new HashSet<Integer>();
-		s.add(1);
-		s.add(10);
-		System.err.println(s);
+	public static void main(String args[]) throws JSONException {
+		JSONObject json = new JSONObject();
+		json.put("name", "nambi");
+		json.put("no", "1234");
+		Iterator<?> keys = json.keys();
+
+		while (keys.hasNext()) {
+			String key = (String) keys.next();
+			System.err.println(json.get(key));
+		}
+		System.err.println(Iterators.size(json.keys()));
 	}
 }
