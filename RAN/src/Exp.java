1baab92fc4229156dc5e9bdfac25d2a8499ea8d9
Nambi <40626463+nambi0915@users.noreply.github.com>
2018-11-22 13:13:25 +0530

    added consumer demo

--- RAN/src/Exp.java
+++ RAN/src/Exp.java
@@ -1,15 +1,50 @@
-import java.io.IOException;
-
-import org.json.JSONArray;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class Exp {
-	public static void main(String args[]) {
-		JSONArray jarray = new JSONArray();
 
-		jarray.put("b");
-		jarray.put("a");
-		jarray.put(0, "c");
-		jarray.put(1, "d");
-		System.err.println(jarray.toString());
+	public static void main(String args[]) {
+
+		forEachAndConsumerDemo();
+
 	}
+
+	private static void forEachAndConsumerDemo() {
+
+		// accept() method implemented using lambda expression
+		Consumer<Integer> consumer = t -> {
+			System.err.println(t + 1);
+			System.err.println(t + 2);
+		};
+
+		Consumer<Integer> consumer2 = t -> {
+			System.err.println("consumer2");
+		};
+		// implementing andThen() method using consumer object
+		Consumer<Integer> consumerWithAndThen = consumer.andThen(i -> System.err.println("printed"));
+		List<Integer> list1 = new ArrayList<Integer>();
+		for (int i = 0; i < 10; i++)
+			list1.add(i + 1);
+
+		for (Integer i : list1) {
+			consumer.accept(i);
+		}
+
+		// traversing temporarily
+		list1.forEach(new Consumer<Integer>() {
+			public void accept(Integer t) {
+				// System.err.println(t + 1);
+			}
+
+		});
+
+		// reusable function
+		list1.forEach(consumerWithAndThen);
+		// System.err.println(list1);
+		list1.forEach(consumer.andThen(consumer2));
+
+	}
+
 }
