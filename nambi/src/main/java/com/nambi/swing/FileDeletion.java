c96e9b9ed4800550b175f886a4cc2ebc37a59414
Nambi <40626463+nambi0915@users.noreply.github.com>
2018-12-24 20:05:27 +0530

    modified swingClass

--- nambi/src/main/java/com/nambi/swing/FileDeletion.java
+++ nambi/src/main/java/com/nambi/swing/FileDeletion.java
@@ -2,17 +2,7 @@ package com.nambi.swing;
 
 import java.io.File;
 
-import javax.swing.*;
-
 public class FileDeletion {
-	public static JFrame deletionWindow = new JFrame("Folder Delete");
-
-	public FileDeletion() {
-		deletionWindow.setSize(500, 500);
-		JLabel label = new JLabel("Selected Directory is " + Home.selectedDirectory);
-		deletionWindow.add(label);
-		deletionWindow.setVisible(true);
-	}
 
 	public void deleteEmptyFiles(File directorySelected) {
 		File[] listOfFiles = directorySelected.listFiles();
--- nambi/src/main/java/com/nambi/swing/Home.java
+++ nambi/src/main/java/com/nambi/swing/Home.java
@@ -9,30 +9,50 @@ import java.io.File;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JList;
 
-public class Home {
+public class Home extends JFrame {
 
-	public static JFrame homeWindow = new JFrame("Folder Select");
-
+	private static final long serialVersionUID = -2035400489179934479L;
 	public static File selectedDirectory = new File("");
 
 	public Home() {
-		JButton button = new JButton("Select");
-		button.setBackground(Color.white);
-		button.setBounds(100, 100, 60, 30);
-		homeWindow.add(button);
-		homeWindow.setSize(300, 300);
-		homeWindow.setLayout(null);
-		// frame.setVisible(true);
-		button.addActionListener(new ActionListener() {
+		JButton delete = new JButton("Delete");
+		final JButton selectDirButton = new JButton("Select Folder");
+
+		selectDirButton.setBackground(Color.white);
+		delete.setBackground(Color.white);
+		selectDirButton.setBounds(0, 100, 200, 40);
+		delete.setBounds(0, 140, 200, 40);
+
+		add(selectDirButton);
+		add(delete);
+
+		setSize(700, 500);
+		setLayout(null);
+		selectDirButton.setFocusPainted(false);
+
+		JList<String> folderList = new JList<String>();
+		folderList.setBounds(210, 50, 450, 175);
+		add(folderList);
+
+		JList<File> selectedfolderList = new JList<File>();
+		selectedfolderList.setBounds(210, 50, 450, 175);
+		add(selectedfolderList);
+
+		JList<String> deletefolderList = new JList<String>();
+		deletefolderList.setBounds(210, 250, 450, 175);
+		add(deletefolderList);
+		selectDirButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JFileChooser chooser = new JFileChooser();
+				chooser.setBounds(210, 50, 300, 500);
+				add(chooser);
 				chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 				if (chooser.showOpenDialog(chooser) == JFileChooser.APPROVE_OPTION) {
+					selectDirButton.setEnabled(false);
 					selectedDirectory = chooser.getSelectedFile();
 					System.out.println("Selected folder: " + chooser.getSelectedFile());
-					homeWindow.setVisible(false);
-					new FileDeletion();
 				}
 			}
 		});
@@ -40,8 +60,9 @@ public class Home {
 	}
 
 	public static void main(String args[]) {
-		new Home();
+		Home homeWindow = new Home();
 		homeWindow.setVisible(true);
+		homeWindow.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 	}
 }
