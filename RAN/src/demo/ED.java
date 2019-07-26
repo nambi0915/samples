964c5de896fe9647ea9f5409717bc28d049ec051
Nambi Murugan <nambi.a@data-aces.com>
2019-07-26 18:18:31 +0530

    Added password encoder

---
+++ RAN/src/demo/ED.java
@@ -0,0 +1,22 @@
+package demo;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+public class ED {
+    public static void main(String[] args) throws IOException {
+	byte[] fileBytes = Files.readAllBytes(Paths.get("resources\\pwd.txt"));
+	String encodedPassword = new String(fileBytes);
+
+	// String encodedStr =
+	// Base64.getEncoder().encodeToString(encodedPassword.getBytes());
+	// String nambi = "TmFtYmk=";
+	// System.out.println(encodedStr);
+
+	String decodedStr = new String(Base64.getDecoder().decode(encodedPassword));
+	System.out.println(decodedStr);
+    }
+}
